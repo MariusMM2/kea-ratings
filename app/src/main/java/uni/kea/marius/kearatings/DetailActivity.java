25142db22b31eb12a4f54aa8180c73decd11990e
marius <cepulameavrei>
2019-03-19 02:45:43 +0100

    added new rating button

--- app/src/main/java/uni/kea/marius/kearatings/DetailActivity.java
+++ app/src/main/java/uni/kea/marius/kearatings/DetailActivity.java
@@ -1,11 +1,15 @@
 package uni.kea.marius.kearatings;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import uni.kea.marius.kearatings.model.RepoItem;
 import uni.kea.marius.kearatings.util.AnimationUtils;
 import uni.kea.marius.kearatings.util.ModelBinding;
@@ -18,6 +22,8 @@ public class DetailActivity extends SingleFragmentActivity {
     private RepoItem mItem;
     private View mItemView;
     private ImageButton mImageButton;
+    private FrameLayout mFragmentLayout;
+    private ImageView mFragmentBackground;
 
     @Override
     protected int getLayoutResId() {
@@ -50,11 +56,29 @@ public class DetailActivity extends SingleFragmentActivity {
         mImageButton = findViewById(R.id.item_expand_button);
         mImageButton.setOnClickListener(v -> onBackPressed());
         AnimationUtils.flip(mImageButton, true);
+
+        mFragmentLayout = findViewById(R.id.fragment_container);
+        mFragmentBackground = findViewById(R.id.background);
     }
 
     @Override
     public void onBackPressed() {
         AnimationUtils.flip(mImageButton, false);
         super.onBackPressed();
+    }
+
+    public void addRating(View view) {
+        int x = (int) (view.getLeft() + view.getWidth() / 2f);
+        int y = (int) (view.getTop() + view.getHeight() / 2f);
+
+        int startRadius = 0;
+        View rootView = findViewById(R.id.root_layout);
+        int endRadius = (int) (Math.hypot(rootView.getWidth(), rootView.getHeight()));
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(mFragmentBackground, x, y, startRadius, endRadius);
+
+        mFragmentBackground.setVisibility(View.VISIBLE);
+        anim.setDuration(getResources().getInteger(android.R.integer.config_longAnimTime));
+        anim.start();
     }
 }
